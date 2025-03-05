619b16ccac2e33fdfa81d0f3d2dd57bd92ed1877
Ery Harinanto <eryharinanto111@gmail.com>
2025-03-05 19:53:12 +0700

    update market-demand css

--- src/app/main/market-demand/market-demand.component.ts
+++ src/app/main/market-demand/market-demand.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-market-demand',
   standalone: true,
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './market-demand.component.html',
   styleUrl: './market-demand.component.css'
 })
